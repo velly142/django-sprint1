d7136576d24fdd69299c5f597c555a233b29b039
Valery Shilyaev <valeroshilyaev@gmail.com>
2025-08-29 17:41:10 +0300

    fixed bugs

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+
 def about(request):
     template = 'pages/about.html'
     return render(request, template, {})
+
 
 def rules(request):
     template = 'pages/rules.html'
